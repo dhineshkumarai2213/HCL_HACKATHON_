534c8cef50e6c554bf89679787635ac070683556
dhineshkumarai2213 <dhineshkumar621315@gmail.com>
2026-05-29 15:59:11 +0530

    Add files via upload

---
+++ FILE_FORMAT.sql
@@ -0,0 +1,15 @@
+USE ROLE ACCOUNTADMIN;
+USE WAREHOUSE HOSPITAL_WH;
+USE DATABASE HOSPITAL_DW;
+USE SCHEMA RAW;
+
+CREATE OR REPLACE FILE FORMAT HOSPITAL_DW.RAW.CSV_FILE_FORMAT
+    TYPE                            = 'CSV'
+    FIELD_DELIMITER                 = ','
+    SKIP_HEADER                     = 1
+    FIELD_OPTIONALLY_ENCLOSED_BY    = '"'
+    NULL_IF                         = ('', 'NULL', 'null', 'N/A', 'n/a', 'None')
+    EMPTY_FIELD_AS_NULL             = TRUE
+    ERROR_ON_COLUMN_COUNT_MISMATCH  = FALSE
+    TRIM_SPACE                      = TRUE
+    COMMENT = 'Standard CSV format for hospital data files';
